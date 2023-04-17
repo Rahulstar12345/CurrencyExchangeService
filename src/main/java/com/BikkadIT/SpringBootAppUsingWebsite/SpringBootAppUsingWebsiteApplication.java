5e31d17624dd2a4a2a21a4e1cf54e585527ef1d9
Rahulstar12345 <rs7350245951@gmail.com>
2023-04-17 23:45:46 +0530

    this is a rahul

---
+++ src/main/java/com/BikkadIT/SpringBootAppUsingWebsite/SpringBootAppUsingWebsiteApplication.java
@@ -0,0 +1,14 @@
+package com.BikkadIT.SpringBootAppUsingWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppUsingWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppUsingWebsiteApplication.class, args);
+		System.out.println("Working Fine");
+	}
+
+}
---
+++ src/test/java/com/BikkadIT/SpringBootAppUsingWebsite/SpringBootAppUsingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.SpringBootAppUsingWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppUsingWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
